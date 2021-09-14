cc994ec96bbd99d415633088c5ff70b83e2891da
CaioNunes <caiofelipe147@gmail.com>
2021-09-13 22:16:26 -0300

    Ajuste de configurações

--- rpgdemesa/rpgdemesa/settings.py
+++ rpgdemesa/rpgdemesa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+
+STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
